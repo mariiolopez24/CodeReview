c384fef9fffdbc4782a4c9f6079e75e5043483b7
Mario Lopez <mario200273@gmail.com>
2026-04-06 15:15:56 +0200

    config: set real Tawk.to property and widget IDs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -97,7 +97,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
             (function(){
               var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
               s1.async=true;
-              s1.src='https://embed.tawk.to/TAWK_PROPERTY_ID/1icu7bhcj';
+              s1.src='https://embed.tawk.to/69d3b1a28085d21c379a3676/1jlheroht';
               s1.charset='UTF-8';
               s1.setAttribute('crossorigin','*');
               s0.parentNode.insertBefore(s1,s0);
